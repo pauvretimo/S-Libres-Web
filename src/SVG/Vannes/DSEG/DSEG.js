a48afb895ad1eac9a36cf17101bb25d69d3ebf18
pauvretimo <73127408+pauvretimo@users.noreply.github.com>
2023-01-17 12:35:26 +0100

    Correction menu return

--- src/SVG/Vannes/DSEG/DSEG.js
+++ src/SVG/Vannes/DSEG/DSEG.js
@@ -5,6 +5,7 @@ import DSEG_E2 from "./DSEG_E2";
 import DSEG_E3 from "./DSEG_E3";
 import "../../../Menu";
 import Menu from "../../../Menu";
+import Return from "../../../Return";
 
 class DSEG extends Batiment {
     constructor(props) {
@@ -22,6 +23,7 @@ class DSEG extends Batiment {
             this.state.isLoading
                 ? <div>load</div> : <div style={{background: "var(--background)", width: "100vw", height: "100vh"}} data-theme={this.state.theme} className="mainDiv">
                     <Menu handleIsNow={this.handleIsNow} handleTheme={this.handleTheme} handleLeft={this.goDown} handleRight={this.goUp}></Menu>
+                    <Return/>
                     <p className={this.state.floor === 0 ? "hidden" : "navArrowLeft navArrow"} onClick={this.goDown}>&#x3c;</p>
                     <p className={this.state.floor === this.floorNumber-1 ? "hidden" : "navArrowRight navArrow"} onClick={this.goUp}>&#x3e;</p>
                     {this.state.floor === 0 && <DSEG_RDC key={"0" + this.state.key.toString()} ical={this.state.ical} isNow={this.state.isNow} start={this.state.start} end={this.state.end}/>}
